b89a404cfbd66ad816702744e8a727ce75b6ed9d
Lukasz 'grzana' Grzenkowicz <lukasz@gembit.pl>
2016-10-19 21:16:30 +0200

    add PowerManager and KeyguardManager

--- src/android/WakeupBootReceiver.java
+++ src/android/WakeupBootReceiver.java
@@ -1,4 +1,4 @@
-package org.nypr.cordova.wakeupplugin;
+package pl.gembit.cordova.wakeupplugin;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
--- src/android/WakeupReceiver.java
+++ src/android/WakeupReceiver.java
@@ -1,4 +1,4 @@
-package org.nypr.cordova.wakeupplugin;
+package pl.gembit.cordova.wakeupplugin;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -10,81 +10,91 @@ import org.json.JSONObject;
 import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.app.KeyguardManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.util.Log;
 
 public class WakeupReceiver extends BroadcastReceiver {
 
-	private static final String LOG_TAG = "WakeupReceiver";
+    private static final String LOG_TAG = "WakeupReceiver";
 
-	@SuppressLint({ "SimpleDateFormat", "NewApi" })
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Log.d(LOG_TAG, "wakeuptimer expired at " + sdf.format(new Date().getTime()));
-	
-		try {
-			String packageName = context.getPackageName();
-			Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
-			String className = launchIntent.getComponent().getClassName();		    	
-			Log.d(LOG_TAG, "launching activity for class " + className);
+    @SuppressLint({"SimpleDateFormat", "NewApi"})
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Log.d(LOG_TAG, "wakeuptimer expired at " + sdf.format(new Date().getTime()));
 
-			@SuppressWarnings("rawtypes")
-			Class c = Class.forName(className); 
+        try {
+            String packageName = context.getPackageName();
+            Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+            String className = launchIntent.getComponent().getClassName();
+            Log.d(LOG_TAG, "launching activity for class " + className);
 
-			Intent i = new Intent(context, c);
-			i.putExtra("wakeup", true);
-			Bundle extrasBundle = intent.getExtras();
-			String extras=null;
-			if (extrasBundle!=null && extrasBundle.get("extra")!=null) {
-				extras = extrasBundle.get("extra").toString();
-			}
-			
-			if (extras!=null) {
-				i.putExtra("extra", extras);
-			}
-			i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			context.startActivity(i);
+            @SuppressWarnings("rawtypes")
+            Class c = Class.forName(className);
 
-			if(WakeupPlugin.connectionCallbackContext!=null) {
-				JSONObject o=new JSONObject();
-				o.put("type", "wakeup");
-				if (extras!=null) {
-					o.put("extra", extras);
-				}
-				PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, o);
-				pluginResult.setKeepCallback(true);
-				WakeupPlugin.connectionCallbackContext.sendPluginResult(pluginResult);  
-			}
-			
-			if (extrasBundle!=null && extrasBundle.getString("type")!=null && extrasBundle.getString("type").equals("daylist")) {
-				// repeat in one week
-				Date next = new Date(new Date().getTime() + (7 * 24 * 60 * 60 * 1000));
-				Log.d(LOG_TAG,"resetting alarm at " + sdf.format(next));
-	
-				Intent reschedule = new Intent(context, WakeupReceiver.class);
-				if (extras!=null) {
-					reschedule.putExtra("extra", intent.getExtras().get("extra").toString());
-				}
-				reschedule.putExtra("day", WakeupPlugin.daysOfWeek.get(intent.getExtras().get("day")));
-	
-				PendingIntent sender = PendingIntent.getBroadcast(context, 19999 + WakeupPlugin.daysOfWeek.get(intent.getExtras().get("day")), intent, PendingIntent.FLAG_UPDATE_CURRENT);
-				AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-				if (Build.VERSION.SDK_INT>=19) {
-					alarmManager.setExact(AlarmManager.RTC_WAKEUP, next.getTime(), sender);
-				} else {
-					alarmManager.set(AlarmManager.RTC_WAKEUP, next.getTime(), sender);
-				}
-			}
+            Intent i = new Intent(context, c);
+            i.putExtra("wakeup", true);
+            Bundle extrasBundle = intent.getExtras();
+            String extras = null;
+            if (extrasBundle != null && extrasBundle.get("extra") != null) {
+                extras = extrasBundle.get("extra").toString();
+            }
 
-		} catch (JSONException e){
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+            if (extras != null) {
+                i.putExtra("extra", extras);
+            }
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+
+            PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+            PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, LOG_TAG);
+            wakeLock.acquire();
+
+            KeyguardManager keyguardManager = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+            KeyguardManager.KeyguardLock keyguardLock = keyguardManager.newKeyguardLock(LOG_TAG);
+            keyguardLock.disableKeyguard();
+
+            if (WakeupPlugin.connectionCallbackContext != null) {
+                JSONObject o = new JSONObject();
+                o.put("type", "wakeup");
+                if (extras != null) {
+                    o.put("extra", extras);
+                }
+                PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, o);
+                pluginResult.setKeepCallback(true);
+                WakeupPlugin.connectionCallbackContext.sendPluginResult(pluginResult);
+            }
+
+            if (extrasBundle != null && extrasBundle.getString("type") != null && extrasBundle.getString("type").equals("daylist")) {
+                // repeat in one week
+                Date next = new Date(new Date().getTime() + (7 * 24 * 60 * 60 * 1000));
+                Log.d(LOG_TAG, "resetting alarm at " + sdf.format(next));
+
+                Intent reschedule = new Intent(context, WakeupReceiver.class);
+                if (extras != null) {
+                    reschedule.putExtra("extra", intent.getExtras().get("extra").toString());
+                }
+                reschedule.putExtra("day", WakeupPlugin.daysOfWeek.get(intent.getExtras().get("day")));
+
+                PendingIntent sender = PendingIntent.getBroadcast(context, 19999 + WakeupPlugin.daysOfWeek.get(intent.getExtras().get("day")), intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+                if (Build.VERSION.SDK_INT >= 19) {
+                    alarmManager.setExact(AlarmManager.RTC_WAKEUP, next.getTime(), sender);
+                } else {
+                    alarmManager.set(AlarmManager.RTC_WAKEUP, next.getTime(), sender);
+                }
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
